423062713c528022a74e30509ef3dfe1eddd1c74
Shruthi <shruthimarri97@gmail.com>
2021-01-13 18:55:42 +0530

    Added: After Multiple flipping Display No. of Heads and Tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,28 @@
 #!/bin/bash -x
 
-flipCoin=$(( RANDOM%2 ))
-
-if [[ $flipCoin -eq 1 ]]
+echo "Enter number of flips :"
+read n
+unit=1
+Heads=0
+Tails=0
+for (( i=1; i<=$n; i++ ))
+do
+	flipCoin=$(( RANDOM%2 ))
+		if [[ $flipCoin -eq 1 ]]
+		then
+			Heads=$(($Heads+$unit))
+		else 
+			 Tails=$(($Tails+$unit))
+		fi
+done
+if [[ $Heads -gt $Tails ]]
 then
-	echo Heads
-else 
-	echo Tails
-fi
+	echo Heads Won
 
+elif [[ $Tails -gt $Heads ]]
+then
+	echo Tails Won
+elif [[ $Heads -eq $Tails ]]
+then
+	echo Tails=Heads
+fi
