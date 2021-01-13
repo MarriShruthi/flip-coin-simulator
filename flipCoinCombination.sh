a895dd1645b7f4216bc5f91a16fe15f395fc71a4
Shruthi <shruthimarri97@gmail.com>
2021-01-13 20:06:37 +0530

    Added: singlet dictionary and percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,37 @@
-#!/bin/bash -x 
-
-n=21
-unit=1
+#!/bin/bash  
+echo "Enter n:"
+read n
 Heads=0
 Tails=0
-for (( i=1; i<=$n; i++ ))
+unit=1
+declare -A coin
+for(( i=1; i<=$n; i++ ))
 do
 	flipCoin=$(( RANDOM%2 ))
 		if [[ $flipCoin -eq 1 ]]
 		then
 			Heads=$(($Heads+$unit))
+			coin[$i]="Heads"
 		else 
-			 Tails=$(($Tails+$unit))
+			 coin[$i]="Tails"
 		fi
 done
+echo "All flips :: " ${coin[@]}
 if [[ $Heads -gt $Tails ]]
 then
 	echo Heads Won
 	Res=$(($Heads-$Tails))
 	echo By $Res Heads Won
+	percentage=$(($Heads*10/100))
+	echo percentage=$percentage
 elif [[ $Tails -gt $Heads ]]
 then
 	echo Tails Won
 	Res=$(($Tails-$Heads))
 	echo By $Res Tails Won
+	 percentage=$(($Tails*10/100))
+        echo percentage=$percentage
+
 elif [[ $Heads -eq $Tails ]]
 then
 	echo Tails and Heads are Tie
