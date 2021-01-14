e8b1196eb623e7e48cb32fbd670bbd632ce80d83
Shruthi <shruthimarri97@gmail.com>
2021-01-14 12:31:08 +0530

    Added : Triplet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo  "enter n :"
 read n
 declare -A coin
@@ -6,21 +7,39 @@ for (( i=1; i<=$n; i++ ))
 do
 	coin1=$(( RANDOM%2 ))
 	coin2=$(( RANDOM%2 ))
-if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
+	coin3=$(( RANDOM%2 ))
+if [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 1 ]]
 then
-	coin[$i]="HH"
+	coin[$i]="HHH"
 
-elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
+elif [[ $coin1 -eq 1 && $coin2 -eq 1 && $coin3 -eq 0 ]]
 then
-        coin[$i]="HT"
+        coin[$i]="HHT"
 
-elif [[ $coin1 -eq o && $coin2 -eq 1 ]]
+elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 1 ]]
 then
-        coin[$i]="TH"
+        coin[$i]="HTH"
 
-elif [[ $coin1 -eq 0 && $coin2 -eq 0 ]]
+elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq 1 ]]
 then
-        coin[$i]="TT"
+        coin[$i]="THH"
+
+elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 0 ]]
+then
+        coin[$i]="TTT"
+
+elif [[ $coin1 -eq 0 && $coin2 -eq 0 && $coin3 -eq 1 ]]
+then
+        coin[$i]="TTH"
+
+elif [[ $coin1 -eq 0 && $coin2 -eq 1 && $coin3 -eq o ]]
+then
+        coin[$i]="THT"
+
+elif [[ $coin1 -eq 1 && $coin2 -eq 0 && $coin3 -eq 0 ]]
+then
+        coin[$i]="HTT"
+
 fi
 done
  echo "Doublet ::"${coin[@]}
