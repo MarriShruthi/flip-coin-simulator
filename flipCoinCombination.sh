1317e40d1a6a10f72e8ac481ce0eaa3ff6ba0658
Shruthi <shruthimarri97@gmail.com>
2021-01-14 12:03:12 +0530

    Added : Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,38 +1,26 @@
-#!/bin/bash  
-echo "Enter n:"
+#!/bin/bash
+echo  "enter n :"
 read n
-Heads=0
-Tails=0
-unit=1
 declare -A coin
-for(( i=1; i<=$n; i++ ))
+for (( i=1; i<=$n; i++ ))
 do
-	flipCoin=$(( RANDOM%2 ))
-		if [[ $flipCoin -eq 1 ]]
-		then
-			Heads=$(($Heads+$unit))
-			coin[$i]="Heads"
-		else 
-			 coin[$i]="Tails"
-		fi
-done
-echo "All flips :: " ${coin[@]}
-if [[ $Heads -gt $Tails ]]
+	coin1=$(( RANDOM%2 ))
+	coin2=$(( RANDOM%2 ))
+if [[ $coin1 -eq 1 && $coin2 -eq 1 ]]
 then
-	echo Heads Won
-	Res=$(($Heads-$Tails))
-	echo By $Res Heads Won
-	percentage=$(($Heads*10/100))
-	echo percentage=$percentage
-elif [[ $Tails -gt $Heads ]]
-then
-	echo Tails Won
-	Res=$(($Tails-$Heads))
-	echo By $Res Tails Won
-	 percentage=$(($Tails*10/100))
-        echo percentage=$percentage
+	coin[$i]="HH"
 
-elif [[ $Heads -eq $Tails ]]
+elif [[ $coin1 -eq 1 && $coin2 -eq 0 ]]
 then
-	echo Tails and Heads are Tie
+        coin[$i]="HT"
+
+elif [[ $coin1 -eq o && $coin2 -eq 1 ]]
+then
+        coin[$i]="TH"
+
+elif [[ $coin1 -eq 0 && $coin2 -eq 0 ]]
+then
+        coin[$i]="TT"
 fi
+done
+ echo "Doublet ::"${coin[@]}
